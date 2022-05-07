60f523bed736d23d52e04afe2fb8184071dc8dc3
e2labup <greciabarandiaran@gmail.com>
2022-05-07 09:18:44 -0500

    update task numbers for new pilot session

--- settings.py
+++ settings.py
@@ -138,7 +138,7 @@ mturk_hit_settings = {
 
 SESSION_CONFIG_DEFAULTS = {
     'real_world_currency_per_point': 0.2,
-    'participation_fee': 5.00,
+    'participation_fee': 10.00,
     'doc': "",
     'mturk_hit_settings': mturk_hit_settings,
 }
