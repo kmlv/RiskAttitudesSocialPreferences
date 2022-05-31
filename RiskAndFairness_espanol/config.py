683c354c667dd57186b63a2a6bcf37c5109e0c45
e2labup <greciabarandiaran@gmail.com>
2022-05-31 05:28:29 -0500

    random 8 sessions for each participant

--- RiskAndFairness_espanol/config.py
+++ RiskAndFairness_espanol/config.py
@@ -34,10 +34,10 @@ shuffle2 = False
 chosen_rounds = []
 
 data = [
-[
+# [
 # {'mode': 'sec_ownrisk', 'm': 33.32, 'p_x': .33, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
 # {'mode': 'sec_ownrisk', 'm': 34.2, 'p_x': .37, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
-# # {'mode': 'sec_ownrisk', 'm': 35.16, 'p_x': .41, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk', 'm': 35.16, 'p_x': .41, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_ownrisk', 'm': 36.23, 'p_x': .45, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_ownrisk', 'm': 37.41, 'p_x': .5, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_ownrisk', 'm': 38.72, 'p_x': .55, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
@@ -85,10 +85,10 @@ data = [
 # {'mode': 'sec_ownrisk', 'm': 77.75, 'p_x': 1.22, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_ownrisk', 'm': 87.21000000000001, 'p_x': 1.49, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_ownrisk', 'm': 98.77, 'p_x': 1.82, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}}
-],
+# ],
 [
-# {'mode': 'sec_1bl_1ch', 'm': 33.32, 'p_x': .33, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
-# {'mode': 'sec_1bl_1ch', 'm': 34.2, 'p_x': .37, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
+{'mode': 'sec_1bl_1ch', 'm': 33.32, 'p_x': .33, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
+{'mode': 'sec_1bl_1ch', 'm': 34.2, 'p_x': .37, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
 # {'mode': 'sec_1bl_1ch', 'm': 35.16, 'p_x': .41, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_1bl_1ch', 'm': 36.23, 'p_x': .45, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_1bl_1ch', 'm': 37.41, 'p_x': .5, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
@@ -139,8 +139,8 @@ data = [
 # {'mode': 'sec_1bl_1ch', 'm': 98.77, 'p_x': 1.82, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}}
 ],
 [
-# {'mode': 'sec_2bl_1ch', 'm': 33.32, 'p_x': .33, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
-# {'mode': 'sec_2bl_1ch', 'm': 34.2, 'p_x': .37, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
+{'mode': 'sec_2bl_1ch', 'm': 33.32, 'p_x': .33, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
+{'mode': 'sec_2bl_1ch', 'm': 34.2, 'p_x': .37, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Fichas en Estado A (50%)', 'y': 'Fichas en Estado B (50%)'}},
 # {'mode': 'sec_2bl_1ch', 'm': 35.16, 'p_x': .41, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_2bl_1ch', 'm': 36.23, 'p_x': .45, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
 # {'mode': 'sec_2bl_1ch', 'm': 37.41, 'p_x': .5, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
@@ -191,8 +191,8 @@ data = [
 # {'mode': 'sec_2bl_1ch', 'm': 98.77, 'p_x': 1.82, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}}
 ],
 [
-# {'mode': 'probability', 'a_x': 91, 'a_y': 5, 'b_x': 5, 'b_y': 31, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
-# {'mode': 'probability', 'a_x': 79, 'a_y': 5, 'b_x': 5, 'b_y': 32, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
+{'mode': 'probability', 'a_x': 91, 'a_y': 5, 'b_x': 5, 'b_y': 31, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
+{'mode': 'probability', 'a_x': 79, 'a_y': 5, 'b_x': 5, 'b_y': 32, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
 # {'mode': 'probability', 'a_x': 70, 'a_y': 5, 'b_x': 5, 'b_y': 34, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'probability', 'a_x': 61, 'a_y': 5, 'b_x': 5, 'b_y': 36, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'probability', 'a_x': 55, 'a_y': 5, 'b_x': 5, 'b_y': 38, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
@@ -221,13 +221,13 @@ data = [
 # {'mode': 'probability', 'a_x': 21, 'a_y': 5, 'b_x': 5, 'b_y': 85, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}}
 ],
 [
-# {'mode': 'det_giv', 'm': 33.32, 'p_x': .33, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
-# {'mode': 'det_giv', 'm': 34.2, 'p_x': .37, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
-# # {'mode': 'det_giv', 'm': 35.16, 'p_x': .41, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
+{'mode': 'det_giv', 'm': 33.32, 'p_x': .33, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
+{'mode': 'det_giv', 'm': 34.2, 'p_x': .37, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu pareja'}},
+# {'mode': 'det_giv', 'm': 35.16, 'p_x': .41, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 36.23, 'p_x': .45, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 37.41, 'p_x': .5, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 38.72, 'p_x': .55, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
-#  {'mode': 'det_giv', 'm': 40.16, 'p_x': .61, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
+# {'mode': 'det_giv', 'm': 40.16, 'p_x': .61, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 41.76, 'p_x': .67, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 43.52, 'p_x': .74, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 45.47, 'p_x': .8200000000000001, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
@@ -271,7 +271,59 @@ data = [
 # {'mode': 'det_giv', 'm': 77.75, 'p_x': 1.22, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 87.21000000000001, 'p_x': 1.49, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}},
 # {'mode': 'det_giv', 'm': 98.77, 'p_x': 1.82, 'p_y': 1, 'prob_a': 50, 'label': {'x': 'Tus fichas', 'y': 'Las fichas de tu compañero'}}
-{'mode': 'sec_ownrisk_fixedother', 'm': 50, 'a':100, 'b':100, 'p_x': 0.6, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'State A (50%)', 'y': 'State B (50%)'}}]
+],
+[
+{'mode': 'sec_ownrisk_fixedother', 'm': 33.32, 'p_x': .33, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+{'mode': 'sec_ownrisk_fixedother', 'm': 34.2, 'p_x': .37, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 35.16, 'p_x': .41, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 36.23, 'p_x': .45, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 37.41, 'p_x': .5, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 38.72, 'p_x': .55, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 40.16, 'p_x': .61, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 41.76, 'p_x': .67, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 43.52, 'p_x': .74, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 45.47, 'p_x': .8200000000000001, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 46.52, 'p_x': .86, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 47.62, 'p_x': .9, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 48.78, 'p_x': .9500000000000001, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 50, 'p_x': 1, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 51.28, 'p_x': 1.05, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 52.63, 'p_x': 1.11, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 54.05, 'p_x': 1.16, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 55.54, 'p_x': 1.22, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 58.75, 'p_x': 1.35, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 62.3, 'p_x': 1.49, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 66.22, 'p_x': 1.65, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 70.55, 'p_x': 1.82, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 75.34, 'p_x': 2.01, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 80.64, 'p_x': 2.23, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 86.49, 'p_x': 2.46, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 92.96000000000001, 'p_x': 2.72, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100.1, 'p_x': 3, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 1.22, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 1.49, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 1.82, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 2.23, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 2.72, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 3.32, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 4.060000000000001, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 100, 'p_x': 4.95, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 20.19, 'p_x': .2, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 24.66, 'p_x': .25, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 30.12, 'p_x': .3, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 36.79, 'p_x': .37, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 44.93, 'p_x': .45, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 54.88, 'p_x': .55, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 67.03, 'p_x': .67, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 81.87000000000001, 'p_x': .8200000000000001, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 54.21, 'p_x': .55, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 58.46, 'p_x': .67, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 63.66, 'p_x': .8200000000000001, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 70, 'p_x': 1, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 77.75, 'p_x': 1.22, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 87.21000000000001, 'p_x': 1.49, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}},
+# {'mode': 'sec_ownrisk_fixedother', 'm': 98.77, 'p_x': 1.82, 'a': 0, 'b': 0, 'p_y': 1, 'prob_a': 50, 'fixed': {'m': 0, 'p_x': 1, 'p_y': 1, 'a': 0}, 'label': {'x': 'Estado A (50%)', 'y': 'Estado B (50%)'}}
+]
 ]
 
 #aleatorizar numero de tareas por bloque
@@ -327,25 +379,22 @@ def shuffle(data):
     shuffled_data = []
 
     # shuffle each block
-    session_1=[1,2,3,5,4]
-    session_2=[1,3,2,5,4]
-    session_3=[1,2,3,4,5]
-    session_4=[1,3,2,4,5]
-    session_5=[5,4,2,3,1]
-    session_6=[5,4,3,2,1]
-    session_7=[4,5,2,3,1]
-    session_8=[4,5,3,2,1]
+    session_1=[2,3,5,4,1]
+    session_2=[3,2,5,4,1]
+    session_3=[2,3,4,5,1]
+    session_4=[3,2,4,5,1]
+    session_5=[4,3,2,1,5]
+    session_6=[4,3,2,1,5]
+    session_7=[3,4,1,2,5]
+    session_8=[3,4,2,1,5]
 
     list_sessions=[session_1, session_2, session_3, session_4, session_5, session_6, session_7, session_8]
-    print('lista:',list_sessions)
     random_session= random.choice(list_sessions)
-    print('sesion:',random_session)
+    print('random_session: ',random_session)
     sorted_data = [i for _,i in sorted(zip(random_session,data))]
-    print('sorted', sorted_data)
 
     for block in sorted_data:
         shuffled_data.append(random.sample(block, k=len(block))) ##randomiza el orden de las tareas por bloque
-    print('shuffled:',shuffled_data)
     return shuffled_data
 
 
